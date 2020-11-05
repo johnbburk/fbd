0cc1456af4c99c770d688aca49bc8b4b07669f21
occam98 <john.burk98@gmail.com>
2020-11-05 00:10:09 -0500

    change arrow location inputs to offset from origin

--- src/ArrowForm.js
+++ src/ArrowForm.js
@@ -1,14 +1,15 @@
 
 import React, {useState} from 'react'
 import Arrow from "./Arrow"
+import {ORIGIN} from "./constants"
 
 export default function ArrowForm({arrows, newArrow}) {
     const[position, setPosition] = useState({x:0, y:0})
-    console.log(`position: ${position.x}, ${position.y}`)
     return(
         <form  onSubmit={ (e) => {
             e.preventDefault();
-            newArrow(arrows => [...arrows, <Arrow x2={position.x} y2={position.y} />])}}>
+            newArrow(arrows => [...arrows, <Arrow x2={parseInt(position.x)+parseInt(ORIGIN.x)} 
+            y2={-parseInt(position.y)+parseInt(ORIGIN.y)} />])}}>
         <label>x: 
         <input type="number" value = {position.x} onChange={e=>{setPosition({...position, x: e.target.value})}}/>
         </label>
